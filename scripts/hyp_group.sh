4be7c4a2929193945b8bbc4330d53d9c7ad0f671
xiaomu698 <141839390+xiaomu698@users.noreply.github.com>
2024-06-05 16:48:16 +0800

    Update hyp_group.sh

--- scripts/hyp_group.sh
+++ scripts/hyp_group.sh
@@ -1,66 +1,51 @@
 #!/bin/zsh
 
-# This script runs hyperopt on all of the main strategies for the specified group and 'pattern'
-# 'pattern' is basically the prefix for the files, e.g. "PCA". Any files beginning with the pattern prefix and "_"
-# will be processed
-
-# NOTE: for now, only runs with the 'sell' space. May add check to if 'buy' is used later
-
-# Strategy list, and associated hyperopt spaces
-#declare -A strat_list=()
-strat_list=()
-
-# current file (must be at top level)
-curr_file="$0"
-
-# default values
+# 设置默认值
 epochs=100
 spaces="buy sell"
-
 num_days=180
 start_date=$(date +"%Y%m%d")
 
+# 函数：根据操作系统和天数设置起始日期
 set_start_date () {
-  # ndays="$1"
-
-  # Get the operating system name
   os=$(uname)
-
-  # Check if the operating system is Darwin (macOS)
   if [ "$os" = "Darwin" ]; then
-    # Use the -j -v option for BSD date command
     start_date=$(date -j -v-${num_days}d +"%Y%m%d")
   else
-    # Use the -d option for GNU date command
-    start_date=$(date -d "${num_days} days ago " +"%Y%m%d")
+    start_date=$(date -d "${num_days} days ago" +"%Y%m%d")
   fi
 }
 
-#get date from num_days days ago
+# 获取起始日期
 set_start_date
 
+# 获取当前日期和时间范围
 today=$(date +"%Y%m%d")
 timerange="${start_date}-${today}"
 download=0
 jobs=0
 lossf="ExpectancyHyperOptLoss"
-#lossf="SharpeHyperOptLoss"
 random_state=$RANDOM
 alt_config=""
 
-# get the number of cores
-num_cores=`sysctl -n hw.ncpu`
+# 获取 CPU 核心数
+if [ "$(uname)" = "Darwin" ]; then
+  num_cores=$(sysctl -n hw.ncpu)
+else
+  num_cores=$(nproc)
+fi
 min_cores=$((num_cores - 2))
 
+# 函数：运行命令并输出结果
 run_cmd () {
   cmd="${1}"
   echo "${cmd}"
   eval ${cmd}
 }
 
-
+# 函数：显示脚本的使用说明
 show_usage () {
-    script=${curr_file}
+    script=$0
     cat << END
 
 Usage: zsh $script [options] <group> <pattern>
@@ -83,22 +68,23 @@ Usage: zsh $script [options] <group> <pattern>
 END
 }
 
+# 函数：检查是否使用 zsh 运行脚本
 check_shell () {
   is_zsh= ; : | is_zsh=1
   if [[ "${is_zsh}" != "1" ]]; then
-    echo""
+    echo ""
     echo "ERR: Must use zsh for this script"
     exit 0
   fi
 }
 
-# echo a line to both stdout and the logfile
+# 函数：输出信息到日志文件和终端
 add_line () {
         echo "${1}" >> $logfile
         echo "${1}"
 }
 
-# run the hyperopt command using the supplied arguments ($1)
+# 函数：运行 Hyperopt 优化并记录日志
 run_hyperopt () {
     add_line "----------------------"
     add_line "${strat}"
@@ -106,29 +92,23 @@ run_hyperopt () {
     add_line ""
     add_line "freqtrade hyperopt ${1}"
     add_line ""
-#   set -x
     cmd="freqtrade hyperopt ${1} --no-color >> $logfile"
     eval ${cmd}
-#   set +x
 }
 
-# process options
-die() { echo "$*" >&2; exit 2; }  # complain to STDERR and exit with error
+# 错误处理函数
+die() { echo "$*" >&2; exit 2; }
 needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
 
-
-#-------------------
-# Main code
-#-------------------
-
+# 检查是否使用 zsh
 check_shell
 
+# 解析命令行参数
 while getopts c:d:e:j:l:n:s:t:-: OPT; do
-  # support long options: https://stackoverflow.com/a/28466267/519360
-  if [ "$OPT" = "-" ]; then   # long option: reformulate OPT and OPTARG
-    OPT="${OPTARG%%=*}"       # extract long option name
-    OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
-    OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
+  if [ "$OPT" = "-" ]; then
+    OPT="${OPTARG%%=*}"
+    OPTARG="${OPTARG#$OPT}"
+    OPTARG="${OPTARG#=}"
   fi
   case "$OPT" in
     c | config )     needs_arg; alt_config="${OPTARG}" ;;
@@ -140,13 +120,13 @@ while getopts c:d:e:j:l:n:s:t:-: OPT; do
     s | spaces )     needs_arg; spaces="${OPTARG}" ;;
     t | timeframe )  needs_arg; timerange="$OPTARG" ;;
     \? )             show_usage; die "Illegal option --$OPT" ;;
-    ??* )            show_usage; die "Illegal option --$OPT" ;;  # bad long option
-    ? )              show_usage; die "Illegal option --$OPT" ;;  # bad short option (error reported via getopts)
+    ??* )            show_usage; die "Illegal option --$OPT" ;;
+    ? )              show_usage; die "Illegal option --$OPT" ;;
   esac
 done
-shift $((OPTIND-1)) # remove parsed options and args from $@ list
+shift $((OPTIND-1))
 
-
+# 检查参数数量
 if [[ $# -ne 2 ]] ; then
   echo ""
   echo "ERR: Missing argument(s)"
@@ -155,43 +135,41 @@ if [[ $# -ne 2 ]] ; then
   exit 0
 fi
 
-
+# 设置变量
 group=$1
 pattern=$2
 
 strat_dir="user_data/strategies"
-script_dir="${strat_dir}/scripts"
 config_dir="${strat_dir}/config"
 group_dir="${strat_dir}/${group}"
 logfile="hyp_${group}_${pattern:gs/*/}.log"
 
+# 设置配置文件路径
 if [[ -n ${alt_config} ]]; then
   config_file="${config_dir}/${alt_config}.json"
 else
   config_file="${config_dir}/config.json"
 fi
 
-if [ ! -f ${config_file} ]; then
+# 检查配置文件是否存在
+if [ ! -f ${config_file} ];then
     echo "config file not found: ${config_file}"
     exit 0
 fi
 
+# 检查策略目录是否存在
 if [ ! -d ${group_dir} ]; then
     echo "Strategy dir not found: ${group_dir}"
     exit 0
 fi
 
-# get the list of files using the pattern identifier
-
-# if there is already a wildcard in the pattern name, just use that, otherwise use pattern as a prefix
-if [[ ${pattern} == *"*"* ]]; then
-  files="${pattern}.py"
-else
-  files="${pattern}_*.py"
-fi
-
-find_result=$( find ${group_dir} -name "${files}" -type f -print0 | xargs -0 basename | sort -h )
-strat_list=( "${(@f)${find_result}}" )
+# 查找匹配的策略文件
+strat_list=()
+for file in ${group_dir}/${pattern}.py; do
+  if [[ -f "$file" ]]; then
+    strat_list+=($(basename "$file" .py))
+  fi
+done
 
 num_files=${#strat_list[@]}
 if [[ $num_files -eq 0 ]]; then
@@ -199,29 +177,22 @@ if [[ $num_files -eq 0 ]]; then
   exit 0
 fi
 
-
-# calculate min trades
-# extract start & end dates from timerange
-# a=("${(@s/-/)timerange}")
-# start=${a[1]} # don't know why it's reversed
-# end=${a[0]}
+# 计算最小交易数
 start=$(echo $timerange | cut -d "-" -f 1)
 end=$(echo $timerange | cut -d "-" -f 2)
-if [ -z "$end" ]; then
+if [ -z "$end" ];then
   end="$(date "+%Y%m%d")"
 fi
 timerange="${start}-${end}"
 
-#echo "timerange:${timerange} start:${start} end:${end}"
-
-# calculate diff
+# 计算时间差
 zmodload zsh/datetime
 diff=$(( ( $(strftime -r %Y%m%d "$end") - $(strftime -r %Y%m%d "$start") ) / 86400 ))
 
-# set min trades based on # days (N per day)
+# 设置最小交易数
 min_trades=$((diff * 2))
 
-
+# 初始化日志文件
 echo "" >$logfile
 add_line ""
 today=`date`
@@ -233,57 +204,50 @@ add_line "Config file: ${config_file}"
 add_line "Strategy dir: ${group_dir}"
 add_line ""
 
-# set up path
+# 设置 PYTHONPATH
 oldpath=${PYTHONPATH}
 export PYTHONPATH="./${group_dir}:./${strat_dir}:${PYTHONPATH}"
 
+# 设置环境变量以忽略 TensorFlow 警告
+export TF_CPP_MIN_LOG_LEVEL=2
 
-
-if [ ${download} -eq 1 ]; then
+# 下载最新数据（如果需要）
+if [ ${download} -eq 1 ];then
     add_line "Downloading latest data..."
     run_cmd "freqtrade download-data  -t 5m --timerange=${timerange} -c ${config_file}"
 fi
 
+# 设置并行作业参数
 jarg=""
-if [ ${jobs} -gt 0 ]; then
+if [ ${jobs} -gt 0 ];then
     jarg="-j ${jobs}"
 else
-  # for kucoin, reduce number of jobs
+  # 对于 kucoin，减少并行作业数
     if [ "$group" = "kucoin" ]; then
       jarg="-j ${min_cores}"
     fi
 fi
 
-
+# 设置 hyperopt 参数
 hargs=" -c ${config_file} ${jarg} --strategy-path ${group_dir} --timerange=${timerange} --hyperopt-loss ${lossf}"
-
-# add a random state so that each strat starts in the same place
 hargs="${hargs} --random-state ${random_state}"
 
-#for strat space in ${(kv)strat_list}; do
-for strat in ${strat_list//.py/}; do
+# 运行每个策略的 Hyperopt
+for strat in ${strat_list}; do
   add_line ""
 
-
-  #  if [[ "${spaces}" == "" ]]; then
-  #    spaces=$space
-  #  fi
-
-  # run main hyperopt
   args="${hargs} --epochs ${epochs} --space ${spaces} -s ${strat} --min-trades ${min_trades} "
   run_hyperopt ${args}
-
 done
 
+# 运行汇总结果的脚本
 echo ""
 python user_data/strategies/scripts/SummariseHyperOptResults.py ${logfile}
 echo ""
 
-
 echo ""
 echo "Full output is in file: ${logfile}:"
 echo ""
-#cat $logfile
 
-# restore PYTHONPATH
+# 恢复 PYTHONPATH
 export PYTHONPATH="${oldpath}"
